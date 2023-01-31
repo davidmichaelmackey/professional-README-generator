85f52d5188908e7da993cab89bed946e9495e21d
David Mackey <davidmackey92@gmail.com>
2023-01-31 04:38:41 -0600

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,78 @@
 // TODO: Include packages needed for this application
 
+const fs = require("fs");
+const inquirer = require("inquirer");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+  {
+    type: "input",
+    name: "title",
+    message: "Project Title:"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Project Description:"
+  },
+  {
+    type: "checkbox",
+    name: "contents",
+    message: "Table of Contents"
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Installation:"
+  },
+  {
+    type: "input",
+    name: "Usage",
+    message: "Usage Information:"
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "License Type:",
+    choices: ["MIT", "GPL", "Apache"]
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Contributing:"
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Test:"
+  },
+  {
+    type: "",
+    name: "",
+    message: ""
+  },
+  {
+    type: "",
+    name: "",
+    message: ""
+  },
+  {
+    type: "",
+    name: "",
+    message: ""
+  },
+  {
+    type: "",
+    name: "",
+    message: ""
+  },
+]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
